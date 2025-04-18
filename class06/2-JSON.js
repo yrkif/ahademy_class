6c1187c5f6d30e75388090f742fe2ca4855cc4af
yrkif <fikry.ma@gmail.com>
2025-04-19 00:10:09 +0800

    JSON + cachingDecorator

---
+++ class06/2-JSON.js
@@ -0,0 +1,41 @@
+'use strict';
+
+//1.JSON.stringify
+{
+  let student = {
+    name: 'John',
+    age: 30,
+    isAdmin: false,
+    courses: ['html', 'css', 'js'],
+    spouse: null,
+  };
+
+  let json = JSON.stringify(student);
+
+  console.log(typeof json); // string
+  console.log(json);
+}
+
+//property ignorance in JSON.stringify
+{
+  let user = {
+    sayHi() {
+      // function/method diabaikan
+      console.log('Hello');
+    },
+    [Symbol('id')]: 123, // key & value in symbolic diabaikan
+    something: undefined, // an undifined value of a property diabaikan
+  };
+
+  console.log(JSON.stringify(user)); // {}
+}
+
+//2.JSON Parse - convert back string JSON to object
+{
+  let json =
+    '{"name": "John", "age": 35, "isAdmin": false, "friends": [0,1,2,3]}';
+
+  let user = JSON.parse(json);
+
+  console.log(user.friends[1]); // 1
+}
---
+++ class07-advance-learning/1-function-execution.js
@@ -0,0 +1,31 @@
+'use strict';
+
+function slow(x) {
+  // misalnya disini task berat yang takes heavy resources
+  console.log(`Called with ${x}`);
+  return x;
+}
+
+function cachingDecorator(func) {
+  let cache = new Map();
+
+  return function (x) {
+    if (cache.has(x)) {
+      // jika ada kunci tersebut di cache
+      return cache.get(x); // baca hasilnya dari cache
+    }
+
+    let result = func(x); // jika tidak, panggil fungsi asli
+
+    cache.set(x, result); // dan simpan hasilnya di cache
+    return result;
+  };
+}
+
+slow = cachingDecorator(slow);
+
+console.log(slow(1)); // slow(1) disimpan di cache dan hasilnya dikembalikan
+console.log('Again: ' + slow(1)); // slow(1) hasilnya diambil dari cache
+
+console.log(slow(2)); // slow(2) disimpan di cache dan hasilnya dikembalikan
+console.log('Again: ' + slow(2)); // slow(2) hasilnya diambil dari cache

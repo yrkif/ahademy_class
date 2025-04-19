adfeca96425b0640df3297a24621118869f7c500
yrkif <fikry.ma@gmail.com>
2025-04-19 11:14:09 +0800

    arrow lexical this

---
+++ class07-advance-learning/3-arrow-lexical-this.js
@@ -0,0 +1,14 @@
+'use strict';
+
+let group = {
+  title: 'Our Group',
+  students: ['John', 'Pete', 'Alice'],
+
+  showList() {
+    this.students.forEach((student) =>
+      console.log(this.title + ': ' + student)
+    );
+  },
+};
+
+group.showList();

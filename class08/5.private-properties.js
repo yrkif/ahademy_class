f9b03608bc5c44ea934dac50d21c375cb84f5be1
yrkif <fikry.ma@gmail.com>
2025-04-25 09:35:14 +0800

    done private properties

--- class08/5.private-properties.js
+++ class08/5.private-properties.js
@@ -17,6 +17,34 @@
   let coffeeMachine = new CoffeeMachine();
 
   // Tidak bisa mengakses properti private dari luar kelas
-  coffeeMachine.#fixWaterAmount(123); // Error
-  coffeeMachine.#waterLimit = 1000; // Error
+  // coffeeMachine.#fixWaterAmount(123); // Error
+  // coffeeMachine.#waterLimit = 1000; // Error
+
+  coffeeMachine.setWaterAmount(150);
+}
+
+//we can use getter setter to access private fields more elegantly
+{
+  class CoffeeMachine {
+    #waterLimit = 200;
+
+    #fixWaterAmount(value) {
+      if (value < 0) return 0;
+      if (value > this.#waterLimit) return this.#waterLimit;
+      return value;
+    }
+
+    get waterLimit() {
+      return this.#waterLimit;
+    }
+
+    set waterLimit(value) {
+      this.#waterLimit = this.#fixWaterAmount(value);
+    }
+  }
+
+  let coffeeMachine = new CoffeeMachine();
+
+  coffeeMachine.waterLimit = 180;
+  console.log(coffeeMachine.waterLimit);
 }
